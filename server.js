adb9e51388fcadd6ca31ec8c6166ac0984a7521b
mmoritz8 <mmoritz0808@gmail.com>
2020-09-23 19:25:52 -0500

    set up routes and added note via postman

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8000;
-app.listen(port, () => { console.log('We are live on ' + port); });
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {}); app.listen(port, () => { console.log('We are live on ' + port); });
